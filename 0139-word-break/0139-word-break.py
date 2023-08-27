e38018f99a34e7bc81b1fb68888811c18d0f3719
Hitham Elbasheir <38975111+hithamalbasheir@users.noreply.github.com>
2023-08-28 01:19:09 +0400

    Time: 43 ms (72.86%), Space: 16.7 MB (11.64%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,14 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        #Recursive approach
-        @cache
+        #Recursive approach wit own memo
+        memo = {}
         def dfs(s):
             if len(s) == 0:
                 return True
+            if s in memo:
+                return memo[s]
             for word in wordDict:
                 if s.startswith(word):
                     if dfs(s[len(word):]):
+                        memo[s] = True
                         return True
+            memo[s] = False
             return False
         return dfs(s)
         

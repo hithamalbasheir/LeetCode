184db4fbc5b8618d5c26a52c9ce243db0660f541
Hitham Elbasheir <38975111+hithamalbasheir@users.noreply.github.com>
2023-11-12 23:25:29 +0400

    Time: 73 ms (55.28%), Space: 16.5 MB (24.7%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,19 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        #Optimized sliding window approach
         if len(s1) > len(s2):
             return False
-        perm = Counter(s1)
-        for i in range(len(s2) - len(s1) + 1):
-            if s2[i] not in perm:
-                continue
-            temp = perm.copy()
-            start = i
-            while start < len(s2) and s2[start] in temp:
-                char = s2[start]
-                temp[char] -= 1
-                if temp[char] == 0:
-                    temp.pop(char)
-                start += 1
-            if not temp:
+        
+        s1_count = Counter(s1)
+        curr_window = {}
+
+        for i, char in enumerate(s2):
+            #Add current char to window counter
+            curr_window[char] = curr_window.get(char, 0) + 1
+
+            #Remove the char on the left out of the window
+            if i >= len(s1):
+                left_char = s2[i - len(s1)]
+                curr_window[left_char] -= 1
+                if curr_window[left_char] == 0:
+                    curr_window.pop(left_char)
+            
+            if curr_window == s1_count:
                 return True
+                
         return False

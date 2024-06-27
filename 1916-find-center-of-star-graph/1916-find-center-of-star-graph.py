a619303123e621c75ba2f0aa70d81b3f305fabdf
Hitham Elbasheir <38975111+hithamalbasheir@users.noreply.github.com>
2024-06-27 09:29:21 +0400

    Time: 623 ms (99.57%), Space: 52.3 MB (58.68%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
@@ -1,5 +1,11 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        #Since we know that there will always be one common number, we can just return the common element between the first two of them by this one liner comparison
-        return edges[0][0] if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1] else edges[0][1]
-        
+        seen = set()
+        for x, y in edges:
+            if x in seen:
+                return x
+            if y in seen:
+                return y
+            seen.add(x)
+            seen.add(y)
+        return -1

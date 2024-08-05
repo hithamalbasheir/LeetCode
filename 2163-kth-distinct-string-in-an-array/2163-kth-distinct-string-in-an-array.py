6db5b481dd1132e38a0028abc4dd8e98d546c562
Hitham Elbasheir <38975111+hithamalbasheir@users.noreply.github.com>
2024-08-05 23:35:20 +0400

    Time: 69 ms (63.58%), Space: 16.8 MB (35.47%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.py
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
         #Hashing
-        res = OrderedDict()
+        res = defaultdict(int)
         for char in arr:
-            res[char] = res.get(char, 0) + 1
+            res[char] += 1
         
         i = 1
         for key in res:

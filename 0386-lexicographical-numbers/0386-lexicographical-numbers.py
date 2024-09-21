8852d60c2f051a97de4c4bc3b4bcb9790ec26750
Hitham Elbasheir <38975111+hithamalbasheir@users.noreply.github.com>
2024-09-21 14:49:50 +0400

    Time: 121 ms (21.53%), Space: 25 MB (20.22%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -5,8 +5,9 @@ class Solution:
             if num > n:
                 return
             res.append(num)
+            num *= 10
             for i in range(0, 10):
-                dfs(int(str(num) + str(i)))
+                dfs(num + i)
         for i in range(1, 10):
             dfs(i)
         return res

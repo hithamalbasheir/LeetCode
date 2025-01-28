66962c821c124802709c686b7688b5594b468156
Hitham Elbasheir <38975111+hithamalbasheir@users.noreply.github.com>
2025-01-28 19:16:40 +0400

    Time: 4 ms (73.89%), Space: 18.5 MB (60.22%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -2,14 +2,11 @@ class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         #Pre-processing - prefix sum, post fix sum
         res = -1
-        minimum_pivot = 1001
         prefix = 0
         sum_of_nums = sum(nums)
         for i, num in enumerate(nums):
             postfix = sum_of_nums - prefix - num
             if prefix == postfix:
-                if num < minimum_pivot:
-                    minimum_pivot = num
-                    res = i
+                return i
             prefix += num
         return res
